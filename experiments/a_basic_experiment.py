4c7a5cfaa701a2bc1132728bb4739d133c74d548
Wesley Heung <wesley@heungs.com>
2018-05-28 19:28:04 -0400

    Set up experiment a to produce a long-lived genotype.

--- experiments/a_basic_experiment.py
+++ experiments/a_basic_experiment.py
@@ -8,8 +8,8 @@ import numpy as np
 from experiments.util import get_empty_data_file
 from genetic import Genome
 from genetic import Runner
-from gym_util.forwarding_wrappers import ForwardingRewardWrapper
 from gym_util import MaxTimestepsWrapper
+from gym_util.forwarding_wrappers import ForwardingRewardWrapper
 from numpy_util import sigmoid, cat_ones
 from snake import Game, DistanceObservationGame, Direction
 
@@ -19,7 +19,7 @@ class ExperimentRunner(Runner):
     def game_constructor() -> gym.Env:
         game = DistanceObservationGame(map_size=(30, 30))
         game = FitnessWrapper(game)
-        game = MaxTimestepsWrapper(game, max_timesteps=100)
+        game = MaxTimestepsWrapper(game, max_timesteps=10000)
         return game
 
     @staticmethod
@@ -40,21 +40,22 @@ class ExperimentRunner(Runner):
 
     @classmethod
     def run_experiment(cls):
-        with open(get_empty_data_file('data.csv'), 'w') as f:
-            r = cls.__new__(cls)
-            r.__init__(num_agents=200, num_champions=20, max_workers=1, info_file=f)
-            generations = 10
-            f_historical = deque(maxlen=10)
+        np.random.seed(12)
 
-            for s in range(1, generations + 1):
-                start_time = time()
-                f = r.single_iteration()
-                end_time = time()
-                f_historical.append(sum(f) / len(f))
-                print(f"Generation {s} \t"
-                      f"Fitness: {f_historical[-1]} (moving avg. {sum(f_historical) / len(f_historical)})\t"
-                      f"Best: {max(f)}\t"
-                      f"in {end_time-start_time} s")
+        r = cls.__new__(cls)
+        r.__init__(num_agents=200, num_champions=20, max_workers=1, info_file_path=get_empty_data_file('data.csv'))
+        generations = 20
+        f_historical = deque(maxlen=10)
+
+        for s in range(1, generations + 1):
+            start_time = time()
+            f = r.single_iteration()
+            end_time = time()
+            f_historical.append(sum(f) / len(f))
+            print(f"Generation {s} \t"
+                  f"Fitness: {f_historical[-1]} (moving avg. {sum(f_historical) / len(f_historical)})\t"
+                  f"Best: {max(f)}\t"
+                  f"in {end_time-start_time} s")
 
 
 class FitnessWrapper(ForwardingRewardWrapper):
@@ -69,9 +70,9 @@ class FitnessWrapper(ForwardingRewardWrapper):
     def reward(self, reward):
         snake_length = len(self.env.snake_tail)
         if snake_length < 10:
-            return self.env.timesteps ** 2 * 2 ** (snake_length - 2)
+            return (self.env.timesteps ** 2) * (2 ** snake_length)
         else:
-            return self.env.timesteps ** 2 * 2 ** 10 * (snake_length - 9)
+            return (self.env.timesteps ** 2) * (2 ** 10) * (snake_length - 7)
 
 
 if __name__ == '__main__':
